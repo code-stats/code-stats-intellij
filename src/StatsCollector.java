dc5cbff6e3763c8b6190bbf409d1ce64dcd2d832
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2016-06-03 07:38:55 +0300

    Try to fix certificate error by bundling the ISRG root with the plugin

--- src/StatsCollector.java
+++ src/StatsCollector.java
@@ -6,6 +6,15 @@ import com.intellij.openapi.editor.actionSystem.TypedAction;
 import com.intellij.openapi.editor.actionSystem.TypedActionHandler;
 import org.jetbrains.annotations.NotNull;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.security.cert.*;
 import java.util.Hashtable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -47,6 +56,8 @@ public class StatsCollector implements ApplicationComponent {
         handler.setOldTypingHandler(oldHandler);
         handler.setStatsCollector(this);
         typedAction.setupHandler(handler);
+
+        installLECACert();
     }
 
     @Override
@@ -97,5 +108,46 @@ public class StatsCollector implements ApplicationComponent {
     public void setApiConfig(final String apiURL, final String apiKey) {
         this.apiURL = apiURL;
         this.apiKey = apiKey;
+    }
+
+    // Code::Stats is using a Let's Encrypt certificate which Java does not trust by default.
+    // We need to add the bundled LE root CA certificate to the trust store before we can send any calls to the server.
+    // See: http://stackoverflow.com/a/34111150
+    private void installLECACert() {
+        try {
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            Path ksPath = Paths.get(System.getProperty("java.home"),
+                    "lib", "security", "cacerts");
+            keyStore.load(Files.newInputStream(ksPath),
+                    "changeit".toCharArray());
+
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            try (InputStream caInput = new BufferedInputStream(
+                    // this files is shipped with the application
+                    StatsCollector.class.getResourceAsStream("isrgrootx1.der"))) {
+                Certificate crt = cf.generateCertificate(caInput);
+                System.out.println("Added Cert for " + ((X509Certificate) crt)
+                        .getSubjectDN());
+
+                keyStore.setCertificateEntry("ISRG root X1", crt);
+            }
+
+            System.out.println("Truststore now trusting: ");
+            PKIXParameters params = new PKIXParameters(keyStore);
+            params.getTrustAnchors().stream()
+                    .map(TrustAnchor::getTrustedCert)
+                    .map(X509Certificate::getSubjectDN)
+                    .forEach(System.out::println);
+            System.out.println();
+
+            TrustManagerFactory tmf = TrustManagerFactory
+                    .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(keyStore);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, tmf.getTrustManagers(), null);
+            SSLContext.setDefault(sslContext);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
